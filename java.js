b6e21cd9e12dbb16ab8d31d31c2f83a8d2e008c1
Jose g hernandez <jose_ghj@hotmail.com>
2023-02-22 11:43:33 -0600

    add header,main and footer styles

--- java.js
+++
@@ -1,3 +0,0 @@
-// alert(`te gustaria traducir esta magnifica pagina??
-//         (Will you like me to translate this page to english😎 ) `);
-// alert("I'm sorry, I'ma not that good YET!!! try next year")
